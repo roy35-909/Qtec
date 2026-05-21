2ae8cb102679ddc6aaf9765bc862905058a74b02
Simanta Roy <office.simantaroy@gmail.com>
2026-05-21 16:32:38 +0600

    added the monitoring tools

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import logging, time
-
+from prometheus_fastapi_instrumentator import Instrumentator
 app = FastAPI()
 logging.basicConfig(level=logging.INFO)
-
+Instrumentator().instrument(app).expose(app)
 class DataPayload(BaseModel):
     key: str
     value: str
